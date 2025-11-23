559dc5fed1c56c2ade1502df14265bede59f9213
aysu <aisuzulfugarly@gmail.com>
2025-11-23 13:28:06 +0400

    update

---
+++ src/aysu2/HomeWork.java
@@ -0,0 +1,20 @@
+package aysu2;
+
+import java.util.Scanner;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        double grossmaash = 2000;
+        double vergifaizi = 0.14;
+        double vergimeblegi = grossmaash * vergifaizi;
+        double xalismaash = grossmaash - vergimeblegi;
+
+        System.out.println("Gross Maash:" + grossmaash + "Azn");
+        System.out.println("Vergi:" + vergifaizi + "Azn");
+        System.out.println("Bize catan Xalis Maash:" + xalismaash + "Azn");
+
+
+
+
+     }
+}
